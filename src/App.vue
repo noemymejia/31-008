896b6b5eeb2b69e9ad16f47ae29fc64911df3408
noemy <noemy49927@gmail.com>
2024-09-07 09:20:26 -0600

    31-008

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <h1>mi app</h1>
+  <hr/>
+  <MiComponente />
+  
+</template>
+<script setup lang="js"></script>
+import { } from './components/MiComponente.vue'
+
+
+<style scoped></style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>primer componente</h1>
+    <section>
+        <h3>numero - {{ numero }}</h3>
+    </section>
+    <button @click="incrementar">+1</button>
+    <button @click="descrementar">-1</button>
+</template>
+<script lang="js" setup>
+import {Computed, ref} from 'vue'
+const numero = ref(0)
+const incrementar = () =>{
+    numero.value++
+}
+const decrementar = () => {
+    numero.value--
+}
+</script>
+<style scoped> 
+h3 {color: red;}
+</style>
